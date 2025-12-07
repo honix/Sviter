9df246b9abecbc209697a0f659c2554a91d0b606
Fyodor Shchukin <fyodor.shchukin@rubius.com>
2025-12-07 13:52:14 +0500

    fix vite hot reload

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -11,6 +11,11 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    watch: {
+      usePolling: true,
+    },
+  },
   test: {
     globals: true,
     environment: 'jsdom',
